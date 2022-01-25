5c736056f9ca2fd1eaa6d6bbf16297b3c4a27e7a
andhac <andhac84@yahoo.com>
2022-01-25 13:02:39 +0530

    complete till Tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,53 @@
 import "./App.css";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import { Redirect, Route } from "react-router-dom";
 
 // Hoc
 import HomeLayoutHoc from "./HOC/Home.hoc";
+import RestaurantLayoutHoc from "./HOC/Restaurant.hoc";
 
 // Pages
 import HomePage from "./Pages/HomePage";
+import RestaurantPage from "./Pages/Restaurantpage";
 
 function App() {
   return (
     <>
-         <HomeLayoutHoc path="/:type" exact component={HomePage} />
-      <HomeLayoutHoc path="/" exact component={HomePage} />
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <HomeLayoutHoc path="/:type" exact component={HomePage} />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id"
+        exact
+        component={RestaurantPage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/overview"
+        exact
+        component={HomePage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/order-online"
+        exact
+        component={HomePage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/reviews"
+        exact
+        component={HomePage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/menu"
+        exact
+        component={HomePage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/photos"
+        exact
+        component={HomePage}
+      />
     </>
   );
 }
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useParams } from "react-router";
+
+//Components
 import Delivery from "../components/Delivery";
+import Dining from "../components/Dining";
+import NightLife from "../components/NightLife";
+import Nutrition from "../components/Nutrition";
 
 function HomePage() {
   const { type } = useParams();
   console.log(type);
   return (
     <>
-      <div className="my-5">{type === "delivery" && <Delivery />}</div>
+      <div className="my-5">
+        {type === "delivery" && <Delivery />}
+        {type === "dining" && <Dining />}
+        {type === "night" && <NightLife/>}
+        {type === "nutri" && <Nutrition/>}
+        </div>
     </>
   );
 }
